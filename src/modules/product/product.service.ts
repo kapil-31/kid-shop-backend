f9b8b3c4e372366309d5ca26725948787401cdd5
Kapil Karki <lotuskapil31@gmail.com>
2026-02-01 22:14:47 +0800

    first commit

--- src/modules/product/product.service.ts
+++ src/modules/product/product.service.ts
@@ -2,7 +2,7 @@ import { prisma } from "lib/prisma";
 import { CreateProductInput } from "./product.schema";
 
 export async function createProduct(data: CreateProductInput) {
-  return await prisma.product.create({
+  return  prisma.product.create({
     data: {
       name: data.name,
       price: data.price,
